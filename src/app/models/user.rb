c880740cbe13c88dad0cb960971b0bdd8d46b869
ryo-7956 <ryoma.hosoya@gmail.com>
2022-05-09 20:49:08 +0900

    https://railstutorial.jp/chapters/sign_up?version=4.2#cha-sign_up

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,13 +1,20 @@
 class User < ApplicationRecord
     # (https://railstutorial.jp/chapters/modeling_users?version=4.2#cha-modeling_users)
+    
+    # リスト6.42:before_saveコールバックの別の実装
+    before_save { email.downcase! }
+    
     # リスト6.31:email属性を小文字に変換してメールアドレスの一意性を保証する
-    before_save { self.email = email.downcase }
+    # before_save { self.email = email.downcase }
 
     # リスト6.16: name属性に長さの検証を追加する
     validates :name,  presence: true, length: { maximum: 50 }
 
     # リスト6.21:メールフォーマットを正規表現で検証する
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    # リスト6.43:有効なメールアドレスかどうか (ドットが２つ以上連続するかどうか) を検証する正規表現
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
     validates :email, presence: true, length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX },
     # リスト6.24:メールアドレスの一意性を検証する
--- src/test/models/user_test.rb
+++ src/test/models/user_test.rb
@@ -66,6 +66,14 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  # リスト6.41:リスト6.31のメールアドレス小文字変換をテストする
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
   # リスト6.38:パスワードの最小文字数をテストする
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
