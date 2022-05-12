357ef13181d74eca689c7dd5cfe84d8b375f849d
ryo-7956 <ryoma.hosoya@gmail.com>
2022-05-12 11:21:45 +0900

    device導入他

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # リスト5.33:ユーザー登録ページのルート(https://railstutorial.jp/chapters/modeling_users?version=4.2#cha-modeling_users)
   get 'signup'  => 'users#new'
   resources :users
-  
 end
